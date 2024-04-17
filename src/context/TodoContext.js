ceaf057023e130bcd61e5c6226b01a1c3a0fbb93
abhishekpm15 <pabhishekm@gmail.com>
2024-04-17 14:18:18 +0530

    localstorage added (test)📝

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const TodoContextProvider = createContext();
@@ -8,15 +8,32 @@ const TodoContext = ({ children }) => {
   const [item, setItem] = useState("");
   const [updateId, setUpdateId] = useState(null);
 
+  useEffect(()=>{
+    const localTodos = localStorage.getItem('todos')
+    console.log(localTodos)
+    if(localTodos){
+      setTodos(JSON.parse(localTodos))
+    }
+    else{
+      setTodos(([]))
+    }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos));
+    console.log('local storage set')
+  },[todos])
+
   const AddItem = () => {
-    if (item.length > 0)
+    if (item.length > 0) {
       setTodos([...todos, { id: uuidv4(), todo: item, time: Date() }]);
+    }
     setItem("");
   };
 
   const UpdateItem = () => {
     const updatedTodo = todos.map((todo) =>
-      todo.id === updateId ? { id: todo.id, todo: item , time: Date()} : todo
+      todo.id === updateId ? { id: todo.id, todo: item, time: Date() } : todo
     );
     setTodos(updatedTodo);
     setItem("");
